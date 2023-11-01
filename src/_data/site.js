4bc3ff96583cb33c48c9b1decd65953e372408ec
Daniel Raniz Raneland <raniz@factor10.com>
2023-11-01 10:05:20 +0100

    Fix the sitemap

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,7 +3,7 @@ module.exports = {
   description: 'Random words about writing software',
   keywords: ['programming', 'software', 'architecture'],
   // your site url without trailing slash
-  url: 'https://code.raniz.blog',
+  url: 'https://raniz.blog',
   // how many posts you want to show for each page
   paginate: 9
   // if you want to add disqus to your site
